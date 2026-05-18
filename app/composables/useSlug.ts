dfe5128d9488a4217710a43083b19de0749b659e
yaliannyg <yaliannysg@gmail.com>
2026-05-18 17:24:28 -0300

    composable - get slug param

---
+++ app/composables/useSlug.ts
@@ -0,0 +1,9 @@
+import { ref, computed } from "vue";
+
+export function useSlug() {
+  const route = useRoute();
+
+  const slugName = computed(() => route.params.slug);
+
+  return { slugName };
+}
