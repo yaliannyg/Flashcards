1b1e38bd436dfe15b69b4f672cd606b9bf835434
yaliannyg <yaliannysg@gmail.com>
2026-05-21 13:01:22 -0300

    populate categories + total amount of cards

--- server/services/card.service.ts
+++ server/services/card.service.ts
@@ -4,19 +4,26 @@ import { toCardDTO } from "../utils/card.mapper";
 import { CategoryModel } from "../models/Categories";
 
 export const createCard = async ({ front, back, category }: CardDTO) => {
-  const a = await CardModel.create({ front, back, category });
+  const a = await CardModel.create({ front, back, category: category.id });
   console.log(a);
   return toCardDTO(a);
 };
 
 export async function getAllCards() {
-  const cards = await CardModel.find();
+  const cards = await CardModel.find().populate("category", "slug name");
   return cards.map(toCardDTO);
 }
 
 export async function getCardsByCategories(slug: string) {
   const category = await CategoryModel.findOne({ slug });
   if (!category) return [];
-  const cards = await CardModel.find({ category: category._id });
+  const cards = await CardModel.find({ category: category._id }).populate(
+    "category",
+    "slug name",
+  );
   return cards.map(toCardDTO);
+}
+
+export async function totalCards() {
+  return await CardModel.countDocuments();
 }
