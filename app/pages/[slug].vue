1b19c503b8a92fcc29304215f08ba81706e572ce
yaliannyg <yaliannysg@gmail.com>
2026-05-21 13:08:57 -0300

    dinamic category pages by slug category

---
+++ app/pages/[slug].vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="size-full">
+    <CardsList v-if="cards" :cards="cards" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCards } from "#imports";
+import { useSlug } from "~/composables/useSlug";
+
+const { cards } = await useCards();
+</script>
+
+<style scoped></style>
