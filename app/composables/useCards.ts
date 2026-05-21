c786ffdb886a1ccabf1612f97da0c70ba1df82f6
yaliannyg <yaliannysg@gmail.com>
2026-05-21 13:03:45 -0300

    composables updated

---
+++ app/composables/useCards.ts
@@ -0,0 +1,25 @@
+import { ref, computed } from "vue";
+
+import { useSlug } from "#imports";
+export function useCards() {
+  const slug = useSlug();
+
+  const {
+    data: cards,
+    status,
+    error,
+  } = useFetch<CardDTO[]>(`/api/cards/${slug.slugName.value}`);
+
+  const { data: totalAmoundCards } = useFetch<number>(`/api/cards/total`);
+
+  const cardsQuestions = computed(() => {
+    console.log(cards.value)
+    return cards.value?.map((card) => card.front) ?? [];
+  });
+
+  return {
+    cards,
+    cardsQuestions,
+    totalAmoundCards,
+  };
+}
