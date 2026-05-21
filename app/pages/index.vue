b9c2ac823e515d324a90ef069dea05c54ee29a1c
yaliannyg <yaliannysg@gmail.com>
2026-05-21 13:07:02 -0300

    home page with card list

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex">
+    <CardsList v-if="cards" :cards="cards" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCards } from "#imports";
+definePageMeta({
+  layout: "default",
+});
+
+const { cards } = useCards();
+</script>
+
+<style scoped></style>
