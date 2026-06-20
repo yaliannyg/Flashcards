45e3267c4d2a0e30c12ce342f2a9ebd77f03a7c3
yaliannyg <yaliannysg@gmail.com>
2026-06-20 17:47:11 -0300

    sidebar links

--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -11,24 +11,42 @@
     </div>
 
     <div class="flex-1 overflow-y-auto px-3 space-y-px">
-      <div
-        v-for="tag in tags"
-        :key="tag.label"
+      <NuxtLink
+        to="/"
         class="flex items-center justify-between px-3 py-2 rounded-lg cursor-pointer group"
         :style="{ transition: 'background 0.15s' }"
       >
         <div class="flex items-center gap-2.5">
           <Tag :size="12" class="text-text-heading" />
           <span class="text-xs text-text-muted font-normal">
-            {{ tag.label }}
+            {{ ALL_TAGS_LABEL }}
           </span>
         </div>
         <span
           class="text-xxs px-1.5 py-0.5 rounded-full bg-surface-muted text-text-muted"
         >
-          {{ tag.count }}
+          {{ totalAmountFlashcards ?? 0 }}
         </span>
-      </div>
+      </NuxtLink>
+      <NuxtLink
+        v-for="tag in tags"
+        :key="tag.id"
+        :to="`/${tag.slug}`"
+        class="flex items-center justify-between px-3 py-2 rounded-lg cursor-pointer group"
+        :style="{ transition: 'background 0.15s' }"
+      >
+        <div class="flex items-center gap-2.5">
+          <Tag :size="12" class="text-text-heading" />
+          <span class="text-xs text-text-muted font-normal">
+            {{ tag.name }}
+          </span>
+        </div>
+        <span
+          class="text-xxs px-1.5 py-0.5 rounded-full bg-surface-muted text-text-muted"
+        >
+          {{ tag.flashcards?.length ?? 0 }}
+        </span>
+      </NuxtLink>
     </div>
 
     <div class="px-4 py-5">
@@ -44,24 +62,14 @@
 
 <script setup lang="ts">
 import { Plus, Tag } from "@lucide/vue";
+import type { TagDTO } from "~~/shared/types/tags.types";
 
-const tags = [
-  { label: "Algorithms", count: 7 },
-  { label: "Biology", count: 3 },
-  { label: "History", count: 5 },
-  { label: "Mathematics", count: 4 },
-  { label: "Geography", count: 6 },
-  { label: "Physics", count: 3 },
-  { label: "Chemistry", count: 5 },
-  { label: "Literature", count: 4 },
-  { label: "Astronomy", count: 2 },
-];
+const ALL_TAGS_LABEL = "All";
+
+const { data: tags } = await useFetch<TagDTO[]>("/api/tags");
+const { data: totalAmountFlashcards } = await useFetch<number>(
+  "/api/flashcards/total",
+);
 </script>
 
 <style scoped></style>
-<!-- onMouseEnter={(e) =>
-              (e.currentTarget.style.backgroundColor = "rgba(255,255,255,0.05)")
-            }
-            onMouseLeave={(e) =>
-              (e.currentTarget.style.backgroundColor = "transparent")
-            } -->
